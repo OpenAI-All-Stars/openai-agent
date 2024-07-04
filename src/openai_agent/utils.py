4e3fb2ad2a898a73c181e77f670f42615026244e
Ilya Chistyakov <ilchistyakov@gmail.com>
2024-07-04 18:17:20 +0300

    minor-fix

--- src/openai_agent/utils.py
+++ src/openai_agent/utils.py
@@ -14,7 +14,7 @@ from colorama import Fore, Style
 def async_command(f: Callable) -> Callable:
     @functools.wraps(f)
     def decorator(*args, **kwargs) -> None:
-        asyncio.get_event_loop().run_until_complete(f(*args, **kwargs))
+        asyncio.run(f(*args, **kwargs))
     return decorator
 
 
