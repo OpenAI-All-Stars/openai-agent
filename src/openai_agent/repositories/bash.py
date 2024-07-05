b5b9271e388da77b71b221dd4975e77824697030
Ilya Chistyakov <ilchistyakov@gmail.com>
2024-07-06 01:52:19 +0300

    use stdin in bash command (#2)

--- src/openai_agent/repositories/bash.py
+++ src/openai_agent/repositories/bash.py
@@ -1,5 +1,9 @@
-from asyncio import create_subprocess_shell, subprocess
 from dataclasses import dataclass
+
+from openai_agent.utils import Process
+
+
+PROC = Process()
 
 
 @dataclass
@@ -7,15 +11,20 @@ class BashResult:
     stdout: str
     stderr: str
 
+    @property
+    def all(self) -> str:
+        return '\n'.join((self.stdout, self.stderr))
 
-async def execute(command: str) -> BashResult:
-    proc = await create_subprocess_shell(
-        command,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    stdout, stderr = await proc.communicate()
+
+async def execute(command: str | None) -> BashResult:
+    global PROC
+    if PROC.was_run:
+        await PROC.user_input(command)
+    else:
+        assert command is not None
+        await PROC.shell(command)
+    stdout, stderr = await PROC.wait()
     return BashResult(
-        stdout=stdout.decode(),
-        stderr=stderr.decode(),
+        stdout=stdout,
+        stderr=stderr,
     )
